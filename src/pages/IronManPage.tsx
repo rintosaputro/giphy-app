d66c946d6b1a9d40ab57fae0eb483c50b4ffb16f
rintosaputro <saputrorinto@gmail.com>
2023-08-02 21:21:28 +0700

    feat(fetch): implement fetch data

--- src/pages/IronManPage.tsx
+++ src/pages/IronManPage.tsx
@@ -1,7 +1,17 @@
+import { useDispatch } from "react-redux";
 import HeaderText from "../components/HeaderText";
 import ListGift from "../components/ListGift";
+import { useEffect } from "react";
+import { asyncReceiveIronMans } from "../states/ironMan/action";
+import { AppDispatch } from "../states";
 
 const IronManPage = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncReceiveIronMans());
+  }, [dispatch]);
+
   return (
     <main className="container p-10 max-w-7xl m-auto">
       <HeaderText>IRON MAN GIPHY</HeaderText>
